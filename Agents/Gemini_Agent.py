c8b70189f3849b55ae368c3df9b6c70fc9ec8661
Max-Potter <mpotter2001@g.ucla.edu>
2025-02-19 13:53:25 -0800

    update before class

--- Agents/Gemini_Agent.py
+++ Agents/Gemini_Agent.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 
-class Gemini_Agent():
+class gem_Agent():
     #basicInstruction = """You are an agent working with 5 other agents. You have each spent some time observing the behavior of John, 
     #a person of interest. You will be given multiple statements about John, and you must evaluate their likelihood based on 
     #your observations and your discussions with other agents in the group. Each agent has made distinct observations.
@@ -16,7 +16,7 @@ class Gemini_Agent():
 
     ###Flow: Init instruction --> observations --> statement instruction --> statements --> init rating
     ###  --> discuss instruction --> final rating --> vote
-    def __init__(self, condition, observations):
+    def __init__(self, observations):
         self.key = os.getenv("GEMINI_KEY")
         self.client = genai.Client(api_key = self.key)
         self.model = "gemini-2.0-flash"
@@ -37,6 +37,6 @@ class Gemini_Agent():
 
     #def store_context(self):
 
-agent1 = Gemini_Agent("Hierarchy","abcdefg")
-rep = agent1.get_response("Which animal is the best in terms of visual appeal? Be brief. The last word of your response MUST be your animal choice in all capitals.")
-print(rep.text)
+#agent1 = Gemini_Agent("Hierarchy","abcdefg", "aaa")
+#rep = agent1.get_response("Which animal is the best in terms of visual appeal? Be brief. The last word of your response MUST be your animal choice in all capitals.")
+#print(rep.text)
--- Testbed.py
+++ Testbed.py
@@ -1,1 +1,19 @@
 import os
+
+import Instructions.instructions
+from Agents.Gemini_Agent import gem_Agent
+
+class trialExecution():
+    #agents -- list [] of all agents
+    #condition -- condition
+    def __init__(self, agents, condition):
+        self.agents = agents
+        self.condition = condition
+
+    def run_1_trial(self):
+        for agent in self.agents:
+            pass
+
+
+
+
